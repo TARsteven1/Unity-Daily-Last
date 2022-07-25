f315b1ca4be934817403633e8e6c94c9fdbd055e
TAR <764521788@qq.com>
2022-07-25 20:26:23 +0800

    Gizmos调试模式

---
+++ Assets/Unity_Skill/Gizmos/ShouGizmosSample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShouGizmosSample : MonoBehaviour
+{
+    public float raderRadius;
+
+    private void OnDrawGizmos()//在edit模式下,每一帧执行
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,raderRadius);
+    }
+    private void OnDrawGizmosSelected()//点选时调用
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, raderRadius);
+        Gizmos.DrawIcon(transform.position+Vector3.up*3, "Head");
+    }
+}
