eb240c3b9b3a0f142198ac19243535b3037d88bc
TAR <764521788@qq.com>
2022-07-25 20:27:48 +0800

    AnimationCurve曲线

---
+++ Assets/Unity_Skill/tween_AnimationCurve/AnimationCurveScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCurveScript : MonoBehaviour
+{
+     public AnimationCurve animationCurve;
+    public Vector3  Endpoint;
+     Vector3 Startpoint;
+    public float TotalTime;
+    public float MaxHeight;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Startpoint = transform.position;
+    }
+    void Start()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlyWithCurve(animationCurve));
+    }
+    IEnumerator FlyWithCurve(AnimationCurve Curve)
+    {
+        float time = 0;
+        Vector3 dir = (Endpoint-Startpoint).normalized;
+        float distance = Vector3.Distance(Endpoint, Startpoint);
+        while (time<TotalTime)
+        {
+            float normalizedTime = time / TotalTime;
+            float sampleRate = Curve.Evaluate(normalizedTime);
+            transform.position = Startpoint + dir * (distance* normalizedTime)+
+                (sampleRate*MaxHeight)*Vector3.up;
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Unity_Skill/tween_AnimationCurve/TweenScale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenScale : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+    public float TotalTime;
+    public float Scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Scale = transform.localScale;
+        StopAllCoroutines();
+        StartCoroutine(ScaleWithCurve(animationCurve));
+    }
+    IEnumerator ScaleWithCurve(AnimationCurve Curve)
+    {
+        float time = 0;
+        
+        while (time < TotalTime)
+        {
+            float normalizedTime = time / TotalTime;
+            float sampleRate = Curve.Evaluate(normalizedTime);
+            transform.localScale= (sampleRate * Scale) * Vector3.one;
+           
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
