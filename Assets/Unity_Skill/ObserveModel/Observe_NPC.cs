ce19eeb2f282a201f07138250b6c33d2c904dd91
TAR <764521788@qq.com>
2022-07-25 20:28:04 +0800

    观察者模式

---
+++ Assets/Unity_Skill/ObserveModel/Observe_NPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observe_NPC : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Observe_Player.PlayerIsDeadDelegate += MoveTowardTop;
+    }
+    void MoveTowardTop()
+    {
+        transform.Translate(Vector3.up);
+    }
+
+    private void OnDisable()
+    {
+        Observe_Player.PlayerIsDeadDelegate -= MoveTowardTop;
+    }
+}
---
+++ Assets/Unity_Skill/ObserveModel/Observe_Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Observe_Player : MonoBehaviour
+{/// <summary>
+/// 通过委托实现观察者模式,让所有人订阅观察者以达到广播的效果
+/// </summary>
+    public int HP = 3;
+    public static event Action PlayerIsDeadDelegate;
+    private void OnDisable()
+    {
+        PlayerIsDeadDelegate?.Invoke();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KillMySelf();
+    }
+    void KillMySelf()
+    {
+        if (HP>0)
+        {
+            HP--;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
