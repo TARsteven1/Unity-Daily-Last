4dc2c2a63c4de46cbfa8f14b03d388a6c449546b
TAR <764521788@qq.com>
2022-07-30 09:59:31 +0800

    Awake顺序

---
+++ Assets/Unity_Skill/AwakeOrder/AwakeOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeOrder : MonoBehaviour
+{
+    //控制脚本初始化的顺序
+
+    //方法1:声明在加载场景前触发!
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoad()
+    {
+        Debug.Log("声明在加载场景前触发!");
+    }
+   
+    //方法2:Editor-ProjectSetting-Script Excute Order中可以设置脚本执行优先级
+}
