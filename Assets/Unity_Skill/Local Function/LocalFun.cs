01f8d61c26095e1b6ed82bae2c9169153eb5742c
TAR <764521788@qq.com>
2022-07-30 09:59:10 +0800

    本地方法

---
+++ Assets/Unity_Skill/Local Function/LocalFun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalFun : MonoBehaviour
+{
+    /// <summary>
+    /// 本地方法,可以放在方法内的方法,可以方便传值,但存在闭包的问题
+    /// </summary>
+  void TestFun()
+    {
+        int a=1;
+        int b=2;
+
+        a = AddNum();
+
+        int AddNum()
+        {
+            return a + b;
+        }
+    }
+}
