75817287c7c70adefa163a819430667ce92f2541
TAR <764521788@qq.com>
2022-07-30 10:00:49 +0800

    Lambda表达式

---
+++ Assets/Unity_Skill/Lambda/LambdaScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaScript : MonoBehaviour
+{
+    Func<int, int, bool> test4Equality = (x, y) => x == y;//返回bool
+    Action<int, int, bool> eerr = (x,y,fasle) =>  x=y;//Action无返回值
+    Predicate<string> isUpper = s => s.Equals(s.ToUpper());//默认返回bool
+
+}
