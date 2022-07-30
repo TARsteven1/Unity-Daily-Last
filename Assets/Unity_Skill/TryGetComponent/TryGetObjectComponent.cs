da4f2df43b740580d5e3e79c1b03b24d7276d645
TAR <764521788@qq.com>
2022-07-30 10:00:15 +0800

    获取组件

---
+++ Assets/Unity_Skill/TryGetComponent/TryGetObjectComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryGetObjectComponent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //代替GetComponent方法,以免找不到组件
+        if (other.TryGetComponent<TurnDestory>(out TurnDestory turnDestory)) turnDestory.TurnDestroy();
+        //if (other.tag=="XXX")
+        //{
+        //    other.GetComponent<TurnDestory>().TurnDestroy();
+        //}
+    }
+}
