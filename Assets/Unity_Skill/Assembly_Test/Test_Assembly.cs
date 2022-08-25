975153c2b0e199ae34f78cd0d183ba6197c58189
TAR <764521788@qq.com>
2022-08-25 10:25:47 +0800

    Unity Assembly Definition

--- Assets/Unity_Skill/Assembly_Test/Test_Assembly.cs
+++ Assets/Unity_Skill/Assembly_Test/Test_Assembly.cs
@@ -1,8 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//No Engine Reference勾选后就不能使用UnityEngine.dll
+//using UnityEngine;
 
-public class Test_Assembly : MonoBehaviour
+
+//Assembly Dedinition Reference后可以访问对应dll中的成员
+using TMPro;
+
+//添加Overidde Reference后可以访问对应dll中的成员,不勾选此项会加载所有的dll
+using LitJson;
+using NetWorkingAPI;
+
+
+
+public class Test_Assembly /*: MonoBehaviour*/
 {
-    
+    public TextMeshPro textMeshPro;
+    public void Test()
+    {
+        PointsHandler.TestDll("XXX");
+        textMeshPro.text="Overidde Reference后可以访问对应dll中的成员";
+        data.Clear();
+    }
+    JsonData data = new JsonData();//写入json格式的数据
+
+
 }
